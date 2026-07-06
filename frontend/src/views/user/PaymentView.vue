05b0fb57041536db1fd40268e4d39bcb7746d88f
Albert Coady <albertlai19771212@gmail.com>
2026-07-06 09:37:11 +0800

    fix: harden easypay custom method validation
    (cherry picked from commit 0dc6e56aae10f9eaacbfa12dbb477fdad9e93eef)

--- frontend/src/views/user/PaymentView.vue
+++ frontend/src/views/user/PaymentView.vue
@@ -1183,6 +1183,7 @@ onMounted(async () => {
         paymentState.value = restored
         paymentPhase.value = 'paying'
         const restoredMethod = normalizeVisibleMethod(restored.paymentType)
+          || (visibleMethods.value[restored.paymentType] ? restored.paymentType : '')
         if (restoredMethod) {
           selectedMethod.value = restoredMethod
         }
